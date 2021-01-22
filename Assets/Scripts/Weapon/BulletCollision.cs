f028ffe3588fcb8db72abd1d13fdacdbf71b07c0
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-01-22 11:51:43 +0100

    Particle on laser impact

--- Assets/Scripts/Weapon/BulletCollision.cs
+++ Assets/Scripts/Weapon/BulletCollision.cs
@@ -2,12 +2,18 @@
 
 public class BulletCollision : MonoBehaviour
 {
+    [SerializeField] private GameObject _impactPrefab;
+
     private void OnTriggerEnter(Collider other)
     {
-        bool notouch = other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Enemy");
+        bool notouch = other.gameObject.CompareTag("Player") && gameObject.CompareTag("Ally Bullet")  || 
+                       other.gameObject.CompareTag("Enemy")  && gameObject.CompareTag("Enemy Bullet") ||
+                       other.gameObject.CompareTag("Ally Bullet") && gameObject.CompareTag("Ally Bullet") ||
+                       other.gameObject.CompareTag("Enemy Bullet") && gameObject.CompareTag("Enemy Bullet");
 
         if (!notouch)
         {
+            GameObject.Instantiate(_impactPrefab, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
