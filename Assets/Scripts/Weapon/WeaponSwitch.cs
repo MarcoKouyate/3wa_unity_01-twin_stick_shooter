12703710eefc231454cc3e55eb46b7db546fe363
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-07-31 22:45:10 +0200

    Swithc Weapon when touching box (2)

---
+++ Assets/Scripts/Weapon/WeaponSwitch.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace TwinStickShooter {
+    public class WeaponSwitch : MonoBehaviour
+    {
+        [SerializeField] private WeaponHandler weaponHandler;
+        [SerializeField] private Weapon[] weapons;
+
+        public void Switch(WeaponType type)
+        {
+            Weapon weapon = GetWeaponOfType(type);
+            
+            if (!weapon) return;
+
+            DisableAllWeapons();
+            Equip(weapon);
+        }
+
+        private void DisableAllWeapons()
+        {
+            foreach (Weapon weapon in weapons)
+            {
+                weapon.gameObject.SetActive(false);
+            }
+        }
+
+
+        private Weapon GetWeaponOfType(WeaponType type)
+        {
+            foreach (Weapon weapon in weapons)
+            {
+                if (weapon.Type == type) return weapon;
+            }
+
+            return null;
+        }
+
+        public void Equip(Weapon weapon)
+        {
+            weapon.gameObject.SetActive(true);
+            weaponHandler.Equip(weapon);
+            Debug.Log(weapon);
+        }
+    }
+}
