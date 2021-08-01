58a6456eda023c38ed41eb7974456588d563e988
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-08-01 16:30:50 +0200

    Weapon Loot Box

---
+++ Assets/RotateOverTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Game {
+    public class RotateOverTime : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Update()
+        {
+            Vector3 rotation = new Vector3(0, speed * Time.deltaTime, 0);
+            transform.Rotate(rotation);
+        }
+    }
+}
