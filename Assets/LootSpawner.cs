618cfaaa1245cb2329ba277335acf5efffe9a265
Marco Kouyaté <marco.kouyate@yahoo.fr>
2022-01-11 18:27:01 +0100

    Dropping Loot System

--- Assets/LootSpawner.cs
+++ Assets/LootSpawner.cs
@@ -3,10 +3,18 @@
 namespace TwinStickShooter {
     public class LootSpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject _lootPrefab;
+        [SerializeField] private LootStruct[] _loots;
+
         public void Spawn()
         {
-            Instantiate(_lootPrefab, transform.position, Quaternion.identity);
+            foreach(LootStruct loot in _loots)
+            {
+                float roll = Random.Range(0f, 1f);
+                if (roll > loot.spawnrate) continue;
+                Debug.Log($"Roll: {roll} ({loot.prefab.name},{loot.spawnrate})");
+                Instantiate(loot.prefab, transform.position, Quaternion.identity);
+                break;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Loot/LootStruct.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace TwinStickShooter {
+
+    [System.Serializable]
+    public struct LootStruct 
+    {
+        public GameObject prefab;
+        
+        [Range(0f,1f)]
+        public float spawnrate;
+    }
+}
