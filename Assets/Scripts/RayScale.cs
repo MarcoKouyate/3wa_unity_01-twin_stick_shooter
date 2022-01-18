1e8791e024788a1c1ea29e220f80d4353ae3fbee
Marco Kouyaté <marco.kouyate@yahoo.fr>
2022-01-18 11:40:47 +0100

    Sniper Aim

---
+++ Assets/Scripts/RayScale.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwinStickShooter {
+    public class RayScale : MonoBehaviour
+    {
+        [SerializeField] float _maxRange;
+        [SerializeField] bool _break = false;
+
+        private void Update()
+        {
+            Scale();
+        }
+
+        private void Scale()
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            float scale = (Physics.Raycast(ray, out hit)) ? hit.distance : _maxRange;
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Mathf.Clamp(scale, 0, _maxRange));
+            if (_break) Debug.Break();
+        }
+    }
+}
